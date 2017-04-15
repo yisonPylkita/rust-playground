bc1fe95ca191cd7320a95a347356b5e34c995408
yisonPylkita <yisonPylkita@gmail.com>
2017-04-15 15:36:48 +0000

    Rust is not the C. Functions doesn't need to be declared before their first usage. Most important ones should be at the begining of the file

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,16 +1,27 @@
 use std::env;
 
-fn get_arithmetical_expression() -> Option<String> {
-    env::args().nth(1)
-}
-
 fn main() {
     let arithmetical_expression = match get_arithmetical_expression() {
         Some(i) => i,
         None => {
+            print_help();
             panic!("Error: Arithmetical expression not found");
         }
     };
 
     println!("Arithmetical expression to calculate: '{}'", arithmetical_expression);
+}
+
+fn print_help() {
+    println!("calculator");
+    println!("Usage: calculator ARITHMETIC_EXPRESSION");
+    println!("where");
+    println!("ARITHMETIC_EXPRESSION\t arithmetic expression to calculate");
+    println!("");
+    println!("Example");
+    println!(r#"calculator "(2 + 2) * 3""#);
+}
+
+fn get_arithmetical_expression() -> Option<String> {
+    env::args().nth(1)
 }
