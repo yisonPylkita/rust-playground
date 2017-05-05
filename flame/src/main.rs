dd108fdb8af196fb92f3bb7448a5c0930884d8de
yisonPylkita <yisonPylkita@gmail.com>
2017-05-05 18:03:00 +0200

    Flame animation based on tiles

--- flame/src/main.rs
+++ flame/src/main.rs
@@ -1,35 +1,48 @@
 extern crate sdl2;
 
-use sdl2::pixels::Color;
+use std::path::Path;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+use sdl2::rect::Point;
+use std::time::Duration;
 
-pub fn main() {
+fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem.window("Flame generator", 640, 480)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
-
-    let mut canvas = window.renderer().build().unwrap();
-
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
+    let window = video_subsystem.window("SDL2", 640, 480)
+        .position_centered().build().unwrap();
+    let mut canvas = window.into_canvas()
+        .accelerated().build().unwrap();
+    let texture_creator = canvas.texture_creator();
+    canvas.set_draw_color(sdl2::pixels::Color::RGBA(0,0,0,255));
+    let mut timer = sdl_context.timer().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
+    let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("assets/flame.bmp")).unwrap();
+    let texture = texture_creator.create_texture_from_surface(&temp_surface).unwrap();
+    let center = Point::new(320,240);
+    let mut source_rect = Rect::new(0, 0, 256, 256);
+    let mut dest_rect = Rect::new(0,0, 256, 256);
+    dest_rect.center_on(center);
 
-    'running: loop {
+    let mut running = true;
+    while running {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
+                Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
+                    running = false;
                 },
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
+
+        let ticks = timer.ticks();
+
+        source_rect.set_x((256 * ((ticks / 100) % 16) ) as i32);
+        canvas.clear();
+        canvas.copy_ex(&texture, Some(source_rect), Some(dest_rect), 10.0, None, true, false).unwrap();
+        canvas.present();
+
+        std::thread::sleep(Duration::from_millis(50));
     }
 }
